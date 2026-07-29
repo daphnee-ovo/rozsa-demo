cded0513369c69473821ea853eca2073aae97150
Xinyue <daphnee.xin@gmail.com>
2026-07-30 03:02:14 +0800

    refactor: remove retired pi and tui sources

--- devtools/pull_all.sh
+++
@@ -1,4 +0,0 @@
-git switch main && git pull
-git switch refactor/tui && git pull
-git switch refactor/model && git pull
-git switch refactor/app && git pull
--- devtools/sync_main_to_branch.sh
+++
@@ -1,7 +0,0 @@
-git fetch origin
-
-git switch main && git pull origin main
-
-git switch refactor/tui && git merge main
-git switch refactor/model && git merge main
-git switch refactor/app && git merge main
